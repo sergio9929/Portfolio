b2331f371557db9013f674316498a092ddf44b2b
sergio <sergio9929@hotmail.com>
2024-12-16 21:49:14 +0100

    improve table styling and format general css

--- components/SectionExperiencia.vue
+++ components/SectionExperiencia.vue
@@ -42,18 +42,26 @@ function pluralize(number, { singular, plural }) {
             <tbody>
                 <tr class="table__row">
                     <td class="table__title">Irontec</td>
-                    <td class="table__info">Enero 2024 - Actualmente ({{ getDateDiff('2024-01-02') }})</td>
+                    <td class="table__info">
+                        <p class="table__date-range">Enero 2024 - Actualmente</p>
+                        <p class="table__date-diff">({{ getDateDiff('2024-01-02') }})</p>
+                    </td>
                     <td class="table__content">
-                        <p>Desarrollo front-end y mantenimiento de aplicaciones híbridas web/móvil (HTML, CSS, TS, Angular,
+                        <p>Desarrollo front-end y mantenimiento de aplicaciones híbridas web/móvil (HTML, CSS, TS,
+                            Angular,
                             Ionic).</p>
                     </td>
                 </tr>
 
                 <tr class="table__row">
                     <td class="table__title">The Last Dock</td>
-                    <td class="table__info">Enero 2023 - Enero 2024 ({{ getDateDiff('2023-01-09', '2024-01-12') }})</td>
+                    <td class="table__info">
+                        <p class="table__date-range">Enero 2023 - Enero 2024</p>
+                        <p class="table__date-diff">({{ getDateDiff('2023-01-09', '2024-01-12') }})</p>
+                    </td>
                     <td class="table__content">
-                        <p>Diseño y desarrollo de páginas web en WordPress usando plantillas personalizadas (PHP, HTML, CSS,
+                        <p>Diseño y desarrollo de páginas web en WordPress usando plantillas personalizadas (PHP, HTML,
+                            CSS,
                             JS) o Elementor, según se requiera.</p>
                         <p>Desarrollo de plugins y soluciones para WordPress.</p>
                         <p>Diseño y desarrollo de pequeñas aplicaciones web para cubrir todas las necesidades de los
@@ -63,7 +71,10 @@ function pluralize(number, { singular, plural }) {
 
                 <tr class="table__row">
                     <td class="table__title">DocPath</td>
-                    <td class="table__info">Enero 2021 - Octubre 2022 (1 año y 9 meses)</td>
+                    <td class="table__info">
+                        <p class="table__date-range">Enero 2021 - Octubre 2022</p>
+                        <p class="table__date-diff">(1 año y 9 meses)</p>
+                    </td>
                     <td class="table__content">
                         <p>Diseño y maquetación de contenidos: presentaciones, documentos, folletos, carteles, ...</p>
                         <p>Diseño, desarrollo y mantenimiento de páginas web:<br />- Wordpress + DIVI<br />- Tema
@@ -76,7 +87,10 @@ function pluralize(number, { singular, plural }) {
 
                 <tr class="table__row">
                     <td class="table__title">Ibermática</td>
-                    <td class="table__info">Septiembre 2020 - Enero 2021 (4 meses)</td>
+                    <td class="table__info">
+                        <p class="table__date-range">Septiembre 2020 - Enero 2021</p>
+                        <p class="table__date-diff">(4 meses)</p>
+                    </td>
                     <td class="table__content">
                         <p>Añadir pequeñas funcionalidades y corregir errores en una gran aplicación web creada para
                             usarse en Internet Explorer.</p>
